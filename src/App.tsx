ed0d9797a87f95e1a423d9358187001bb7a7b6fa
Krzysztof K <github@krzysztof.kornakiewi.cz>
2024-09-08 00:41:34 +0200

    continue

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,38 @@ import { useDispatch } from "react-redux"
 import "./App.css"
 import KeplerGl from "@kepler.gl/components"
 import { useEffect, useState } from "react"
-import { addDataToMap } from "@kepler.gl/actions";
+import { addDataToMap } from "@kepler.gl/actions"
 
 async function httpGet(url: string) {
   const data = await fetch(url, {})
   return await data.json()
 }
 
-const DATA_URL = "http://ey.mm.st/edges.json"
-
-const sampleConfig = {
-  visState: {
-    filters: [
-      {
-        id: "dateFilter_id",
-        dataId: "date_id",
-        name: "", //the name of the column to be filtered
-        type: "timeRange", //filter type to be use
-      },
-    ],
-  },
+const sampleTripData = {
+  fields: [
+    {
+      name: "tpep_pickup_datetime",
+      format: "YYYY-M-D H:m:s",
+      type: "timestamp",
+    },
+    { name: "pickup_longitude", format: "", type: "real" },
+    { name: "pickup_latitude", format: "", type: "real" },
+  ],
+  rows: [
+    ["2015-01-15 19:05:39 +00:00", -73.99389648, 40.75011063],
+    ["2015-01-15 19:05:39 +00:00", -73.97642517, 40.73981094],
+    ["2015-01-15 19:05:40 +00:00", -73.96870422, 40.75424576],
+  ],
 }
+
+const DATA_URL = "http://ey.mm.st/edges.json"
 
 export default function App() {
   const dispatch = useDispatch()
   const [data, setData] = useState()
 
   const fetchData = async () => {
-    setData(await httpGet(DATA_URL))
+    setData([])
   }
 
   useEffect(() => {
@@ -48,11 +52,19 @@ export default function App() {
             data: data,
           },
           options: {
-            centerMap: true,
-            readOnly: false,
+            centerMap: false,
+            readOnly: true,
           },
-          config: { mapStyle: {styleType: 'light'}}
-        }),
+          config: {
+            mapStyle: { styleType: "light" },
+            mapState: {
+              latitude: 52.253971373306165,
+              longitude: -2.6899063817571394,
+              zoom: 6.655984704565685,
+              isSplit: false,
+            },
+          },
+        })
       )
   }, [dispatch, data])
 
@@ -61,7 +73,7 @@ export default function App() {
       id="map"
       width={window.innerWidth}
       height={window.innerHeight}
-      mapboxApiAccessToken="Your Mapbox Access Token "
+      mapboxApiAccessToken=""
     />
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import fixReactVirtualized from 'esbuild-plugin-react-virtualized'
-
+import commonjs from 'vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), commonjs()],
   server: {
     open: true,
   },
@@ -20,4 +20,5 @@ export default defineConfig({
       plugins: [fixReactVirtualized],
     },
   },
+  define: {'process.env': {}}
 })
