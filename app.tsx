1007e94678d5f21d3f2d6c3537d9017fc18f6997
Krzysztof K <github@krzysztof.kornakiewi.cz>
2024-09-13 12:52:12 +0200

    fix colors

--- app.tsx
+++ app.tsx
@@ -16,189 +16,190 @@ import { GeoJsonLayer } from "deck.gl"
 import { MapboxOverlay, MapboxOverlayProps } from "@deck.gl/mapbox"
 import "maplibre-gl/dist/maplibre-gl.css"
 import {
-	NodeFeature,
-	EdgeFeature,
-	NodeProperties,
-	EdgeProperties,
+  NodeFeature,
+  EdgeFeature,
+  NodeProperties,
+  EdgeProperties,
 } from "./types.tsx"
 import Menu from "./menu.js"
 
 const url_edges =
-	"https://raw.githubusercontent.com/kkornakiewicz/walks-fe/main/edges.json"
+  "https://raw.githubusercontent.com/kkornakiewicz/walks-fe/main/edges.json"
 const url_nodes =
-	"https://raw.githubusercontent.com/kkornakiewicz/walks-fe/main/nodes.json"
+  "https://raw.githubusercontent.com/kkornakiewicz/walks-fe/main/nodes.json"
 
 const INITIAL_VIEW_STATE = {
-	latitude: 41.38685633118305,
-	longitude: 2.1696874299405295,
-	zoom: 13,
-	bearing: 0,
-	pitch: 30,
+  latitude: 41.38685633118305,
+  longitude: 2.1696874299405295,
+  zoom: 13,
+  bearing: 0,
+  pitch: 30,
 }
 
-const MAP_STYLE = "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
 
 function DeckGLOverlay(props: MapboxOverlayProps) {
-	const overlay = useControl(() => new MapboxOverlay(props))
-	overlay.setProps(props)
-	return null
+  const overlay = useControl(() => new MapboxOverlay(props))
+  overlay.setProps(props)
+  return null
 }
 
 function Root() {
-	const [opened, { toggle, open, close }] = useDisclosure(false)
-	const [showStreets, setShowStreets] = useState(true)
-	const [showNodes, setShowNodes] = useState(false)
+  const [opened, { toggle, open, close }] = useDisclosure(false)
+  const [showStreets, setShowStreets] = useState(true)
+  const [showNodes, setShowNodes] = useState(false)
 
-	const [selectedEdge, setSelectedEdge] = useState<EdgeFeature | null>(null)
-	const [edges, setEdges] = useState<GeoJSON.FeatureCollection<
-		GeoJSON.LineString,
-		EdgeProperties
-	> | null>(null)
-	const [nodes, setNodes] = useState<GeoJSON.FeatureCollection<
-		GeoJSON.Point,
-		NodeProperties
-	> | null>(null)
-	const [loading, setLoading] = useState(true)
-	const [error, setError] = useState(null)
-	const [hovered, setHovered] = useState(null)
+  const [selectedEdge, setSelectedEdge] = useState<EdgeFeature | null>(null)
+  const [edges, setEdges] = useState<GeoJSON.FeatureCollection<
+    GeoJSON.LineString,
+    EdgeProperties
+  > | null>(null)
+  const [nodes, setNodes] = useState<GeoJSON.FeatureCollection<
+    GeoJSON.Point,
+    NodeProperties
+  > | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [hovered, setHovered] = useState(null)
 
-	const hover = el => {
-		if (el != null) {
-			setHovered(el.properties.osmid)
-		} else {
-			setHovered(null)
-		}
-	}
+  const hover = el => {
+    if (el != null) {
+      setHovered(el.properties.osmid)
+    } else {
+      setHovered(null)
+    }
+  }
 
-	const getEdgeColorByProperty = (el: EdgeFeature) => {
-		if (el.properties.osmid === hovered) {
-			return [112, 41, 99]
-		}
-		if (el.properties.visited === true) return [250, 128, 114, 140]
-		else {
-			return [250, 128, 114, 10]
-		}
-	}
+  const getEdgeColorByProperty = (el: EdgeFeature) => {
+    if (el.properties.osmid === hovered) {
+      return [224, 49, 49, 240]
+    }
+    if (el.properties.visited === true) return [0, 47, 167, 140]
+    else {
+      return [0, 47, 167, 10]
+    }
+  }
 
-	const getNodeColorByProperty = (el: NodeFeature) => {
-		if (el.properties.visited === true) {
-			return [0, 0, 0, 20]
-		}
-		return [0, 0, 0, 180]
-	}
+  const getNodeColorByProperty = (el: NodeFeature) => {
+    if (el.properties.visited === true) {
+      return [0, 0, 0, 20]
+    }
+    return [0, 0, 0, 180]
+  }
 
-	useEffect(() => {
-		// Fetch data when the component loads
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url_edges)
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`)
-				}
-				const jsonData = await response.json() // Use response.text() for non-JSON data
-				setEdges(jsonData)
-				setLoading(false)
-			} catch (error) {
-				setError(error.message)
-				setLoading(false)
-			}
-		}
+  useEffect(() => {
+    // Fetch data when the component loads
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url_edges)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const jsonData = await response.json() // Use response.text() for non-JSON data
+        setEdges(jsonData)
+        setLoading(false)
+      } catch (error) {
+        setError(error.message)
+        setLoading(false)
+      }
+    }
 
-		fetchData()
-	}, [])
+    fetchData()
+  }, [])
 
-	useEffect(() => {
-		// Fetch data when the component loads
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url_nodes)
-				if (!response.ok) {
-					throw new Error(`HTTP error! Status: ${response.status}`)
-				}
-				const jsonData = await response.json() // Use response.text() for non-JSON data
-				setNodes(jsonData)
-				console.log(jsonData)
-				setLoading(false)
-			} catch (error) {
-				setError(error.message)
-				setLoading(false)
-			}
-		}
+  useEffect(() => {
+    // Fetch data when the component loads
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url_nodes)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+        const jsonData = await response.json() // Use response.text() for non-JSON data
+        setNodes(jsonData)
+        console.log(jsonData)
+        setLoading(false)
+      } catch (error) {
+        setError(error.message)
+        setLoading(false)
+      }
+    }
 
-		fetchData()
-	}, [])
+    fetchData()
+  }, [])
 
-	const layers = [
-		new GeoJsonLayer({
-			id: "map",
-			data: edges,
-			// Styles
-			getLineColor: getEdgeColorByProperty,
-			getLineWidth: 8,
-			// Interactive props
-			autoHighlight: true,
-			pickable: true,
-			onClick: info => setSelectedEdge(info.object),
-			onHover: info => hover(info.object),
-			updateTriggers: {
-				getLineColor: [hovered],
-			},
-			beforeId: "watername_ocean", // In interleaved mode, render the layer under map labels
-			visible: showStreets,
-		}),
-		new GeoJsonLayer({
-			id: "nodes",
-			data: nodes,
-			// Styles
-			getLineColor: getNodeColorByProperty,
-			// Interactive props
-			autoHighlight: true,
-			getLineWidth: 10,
-			beforeId: "watername_ocean", // In interleaved mode, render the layer under map labels
-			visible: showNodes
-		}),
-	]
+  const layers = [
+    new GeoJsonLayer({
+      id: "map",
+      data: edges,
+      // Styles
+      getLineColor: getEdgeColorByProperty,
+      getLineWidth: 8,
+      // Interactive props
+      autoHighlight: true,
+      pickable: true,
+      onClick: info => setSelectedEdge(info.object),
+      onHover: info => hover(info.object),
+      updateTriggers: {
+        getLineColor: [hovered],
+      },
+      beforeId: "watername_ocean", // In interleaved mode, render the layer under map labels
+      visible: showStreets,
+    }),
+    new GeoJsonLayer({
+      id: "nodes",
+      data: nodes,
+      // Styles
+      getLineColor: getNodeColorByProperty,
+      // Interactive props
+      autoHighlight: true,
+      getLineWidth: 10,
+      beforeId: "watername_ocean", // In interleaved mode, render the layer under map labels
+      visible: showNodes,
+    }),
+  ]
 
-	return (
-		<MantineProvider>
-			<AppShell header={{ height: 60 }}>
-				<AppShell.Header withBorder={false}>
-					<Header opened={opened} toggle={toggle} />
-				</AppShell.Header>
+  return (
+    <MantineProvider>
+      <AppShell header={{ height: 60 }}>
+        <AppShell.Header withBorder={false}>
+          <Header opened={opened} toggle={toggle} />
+        </AppShell.Header>
 
-				<AppShell.Main>
-					<Menu
-						opened={opened}
-						close={close}
-						open={open}
-						showStreets={showStreets}
-						setShowStreets={setShowStreets}
-						showNodes={showNodes}
-						setShowNodes={setShowNodes}
-					/>
-					<Container h="100vh" fluid w="100%" p="0">
-						<Map initialViewState={INITIAL_VIEW_STATE} mapStyle={MAP_STYLE}>
-							{selectedEdge && (
-								<Popup
-									key={selectedEdge.properties.osmid}
-									style={{ zIndex: 10 }} /* position above deck.gl canvas */
-									longitude={selectedEdge.geometry.coordinates[0][0]}
-									latitude={selectedEdge.geometry.coordinates[0][1]}
-									maxWidth="60em"
-								>
-									<Text>{selectedEdge.properties.name}</Text>
-								</Popup>
-							)}
-							<DeckGLOverlay
-								layers={layers}
-								/* interleaved */ useDevicePixels={false}
-							/>
-						</Map>
-					</Container>
-				</AppShell.Main>
-			</AppShell>
-		</MantineProvider>
-	)
+        <AppShell.Main>
+          <Menu
+            opened={opened}
+            close={close}
+            open={open}
+            showStreets={showStreets}
+            setShowStreets={setShowStreets}
+            showNodes={showNodes}
+            setShowNodes={setShowNodes}
+          />
+          <Container h="100vh" fluid w="100%" p="0">
+            <Map initialViewState={INITIAL_VIEW_STATE} mapStyle={MAP_STYLE}>
+              {selectedEdge && (
+                <Popup
+                  key={selectedEdge.properties.osmid}
+                  style={{ zIndex: 10 }} /* position above deck.gl canvas */
+                  longitude={selectedEdge.geometry.coordinates[0][0]}
+                  latitude={selectedEdge.geometry.coordinates[0][1]}
+                  maxWidth="60em"
+                >
+                  <Text>{selectedEdge.properties.name}</Text>
+                </Popup>
+              )}
+              <DeckGLOverlay
+                layers={layers}
+                /* interleaved */ useDevicePixels={false}
+              />
+            </Map>
+          </Container>
+        </AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  )
 }
 
 /* global document */
